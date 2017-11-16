be9dc9371b12e623cb901678c9fbbe1446b7805c
Anush Shrestha <ashres13@asu.edu>
2017-11-16 09:10:42 -0700

    Modfiy interfaces

--- crypto2017/cipher.h
+++ crypto2017/cipher.h
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-vector<vector<int>> ShiftRows(vector <vector <int>> state, int Nb);
-vector<vector<int>> SubBytes(vector <vector <int>> state, int Nb);
-vector<vector<int>> KeyExpansion(vector <int> key, int Nb);
-extern vector <vector <int>> sbox;
+void ShiftRows(int ** state, int Nb);
+void SubBytes(int ** state, int Nb);
+int ** KeyExpansion(int * key, int Nb);
+extern int ** sbox;
