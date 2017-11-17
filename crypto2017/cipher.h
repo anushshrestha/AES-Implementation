4b54d182f390564d7bdf75871d39bb46c5888b94
Anush Shrestha <ashres13@asu.edu>
2017-11-17 09:36:23 -0700

    Update key expansion and shift rows

--- crypto2017/cipher.h
+++ crypto2017/cipher.h
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-void ShiftRows(int ** state, int Nb);
-void SubBytes(int ** state, int Nb);
-int ** KeyExpansion(int * key, int Nb);
-extern int ** sbox;
+void ShiftRows(int state[][4]);
+void SubBytes(int state[][4]);
+int * KeyExpansion(int * key, int Nb);
+extern int sbox[256];
--- crypto2017/shiftrows.cpp
+++ crypto2017/shiftrows.cpp
@@ -6,20 +6,20 @@
 #include "stdio.h"
 using namespace std;
 
-void ShiftRows(int ** state)
+void ShiftRows(int state[][4])
 {
 	int Nb = 4;
-	vector <int> temp;
+	int temp[4];
+	// No need to shift first row.
 	for (int row = 1; row < Nb; row++) {
 		for (int column = 0; column < Nb; column++) {
-			temp.push_back(state[row][(column + row) % Nb]);
+			// As column value iterates, the sequence starts with 1,2,3
+			temp[column] = state[row][(column + row) % Nb];
 		}
 		for (int column = 0; column < Nb; column++) {
 			state[row][column] = temp[column];
 		}
-		temp.clear();
 	}
-	return state;
 }
 
 
